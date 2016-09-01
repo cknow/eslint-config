21cf104b9eb97f41b54e125a005b14183fff3087
Ricardo Gobbo <ricardo@clicknow.com.br>
2016-09-01 11:22:35 -0300

    feat(rules): Add multiple rules in plugin protractor

--- plugins/protractor.js
+++ plugins/protractor.js
@@ -8,17 +8,31 @@ module.exports = {
         'protractor'
     ],
     rules: {
+        'protractor/array-callback-return': 'error',
         'protractor/by-css-shortcut': 'error',
+        'protractor/correct-chaining': 'error',
         'protractor/missing-perform': 'error',
         'protractor/missing-wait-message': 'error',
+        'protractor/no-absolute-url': 'error',
+        'protractor/no-angular-attributes': 'error',
         'protractor/no-angular-classes': 'error',
+        'protractor/no-bootstrap-classes': 'error',
         'protractor/no-browser-pause': 'error',
         'protractor/no-browser-sleep': 'error',
         'protractor/no-by-xpath': 'error',
         'protractor/no-describe-selectors': 'error',
+        'protractor/no-execute-script': 'error',
+        'protractor/no-expect-in-po': 'error',
+        'protractor/no-get-in-it': 'error',
+        'protractor/no-get-inner-outer-html': 'error',
+        'protractor/no-invalid-selectors': 'error',
+        'protractor/no-promise-in-if': 'error',
+        'protractor/no-repetitive-locators': 'error',
+        'protractor/no-repetitive-selectors': 'error',
         'protractor/no-shadowing': 'error',
         'protractor/use-angular-locators': 'error',
         'protractor/use-first-last': 'error',
+        'protractor/use-promise-all': 'error',
         'protractor/use-simple-repeaters': 'error'
     }
 };
