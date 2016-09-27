3b56505ee0d5029ab6202e80e0dc70c2ce486dbe
Ricardo Gobbo <ricardo@clicknow.com.br>
2016-09-27 16:35:37 -0300

    feat(rules): Add `no-unpublished-bin` and `exports-style` in plugins node

--- plugins/node.js
+++ plugins/node.js
@@ -5,9 +5,13 @@ module.exports = {
         'node'
     ],
     rules: {
+        'node/exports-style': ['error', 'module.exports', {
+            allowBatchAssign: false
+        }],
         'node/no-deprecated-api': 'error',
         'node/no-missing-import': 'error',
         'node/no-missing-require': 'error',
+        'node/no-unpublished-bin': 'error',
         'node/no-unpublished-import': 'error',
         'node/no-unpublished-require': 'error',
         'node/no-unsupported-features': ['error', {
