4a2802e3e730f0359add89abe3693c22fb37a871
Ricardo Gobbo <ricardo@clicknow.com.br>
2016-11-03 08:44:07 -0200

    fix(rules): Remove default options for plugin node

--- plugins/node.js
+++ plugins/node.js
@@ -5,18 +5,14 @@ module.exports = {
         'node'
     ],
     rules: {
-        'node/exports-style': ['error', 'module.exports', {
-            allowBatchAssign: false
-        }],
+        'node/exports-style': ['error', 'module.exports'],
         'node/no-deprecated-api': 'error',
         'node/no-missing-import': 'error',
         'node/no-missing-require': 'error',
         'node/no-unpublished-bin': 'error',
         'node/no-unpublished-import': 'error',
         'node/no-unpublished-require': 'error',
-        'node/no-unsupported-features': ['error', {
-            version: 6
-        }],
+        'node/no-unsupported-features': 'error',
         'node/process-exit-as-throw': 'error',
         'node/shebang': 'error'
     }
