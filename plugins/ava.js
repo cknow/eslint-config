a1d647379d18f18122085dbd9bf8bcdd8fccf278
Ricardo Gobbo <ricardo@clicknow.com.br>
2017-01-17 13:50:13 -0200

    feat(rules): Remove rule `ava/assertion-message`

--- plugins/ava.js
+++ plugins/ava.js
@@ -6,7 +6,6 @@ module.exports = {
     ],
     rules: {
         'ava/assertion-arguments': 'error',
-        'ava/assertion-message': ['off', 'always'],
         'ava/max-asserts': ['off', 5],
         'ava/no-async-fn-without-await': 'error',
         'ava/no-cb-test': 'off',
