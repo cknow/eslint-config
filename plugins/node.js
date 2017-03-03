f5a1da2c62ce02bff29657377552ecd02a448476
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2017-03-03 15:15:37 -0300

    feat(rules): Add rule `node/no-hide-core-modules` and explicit other options

--- plugins/node.js
+++ plugins/node.js
@@ -5,15 +5,47 @@ module.exports = {
         'node'
     ],
     rules: {
-        'node/exports-style': ['error', 'module.exports'],
-        'node/no-deprecated-api': 'error',
-        'node/no-missing-import': 'error',
-        'node/no-missing-require': 'error',
-        'node/no-unpublished-bin': 'error',
-        'node/no-unpublished-import': 'error',
-        'node/no-unpublished-require': 'error',
-        'node/no-unsupported-features': 'error',
+        'node/exports-style': ['error', 'module.exports', {
+            allowBatchAssign: false
+        }],
+        'node/no-deprecated-api': ['error', {
+            ignoreModuleItems: [],
+            ignoreGlobalItems: [],
+            ignoreIndirectDependencies: false
+        }],
+        'node/no-hide-core-modules': ['error', {
+            allow: [],
+            ignoreDirectDependencies: false,
+            ignoreIndirectDependencies: false
+        }],
+        'node/no-missing-import': ['error', {
+            allowModules: [],
+            tryExtensions: ['.js', '.json', '.node']
+        }],
+        'node/no-missing-require': ['error', {
+            allowModules: [],
+            tryExtensions: ['.js', '.json', '.node']
+        }],
+        'node/no-unpublished-bin': ['error', {
+            convertPath: {}
+        }],
+        'node/no-unpublished-import': ['error', {
+            allowModules: [],
+            convertPath: {},
+            tryExtensions: ['.js', '.json', '.node']
+        }],
+        'node/no-unpublished-require': ['error', {
+            allowModules: [],
+            convertPath: {},
+            tryExtensions: ['.js', '.json', '.node']
+        }],
+        'node/no-unsupported-features': ['error', {
+            version: 4,
+            ignores: []
+        }],
         'node/process-exit-as-throw': 'error',
-        'node/shebang': 'error'
+        'node/shebang': ['error', {
+            convertPath: {}
+        }]
     }
 };
