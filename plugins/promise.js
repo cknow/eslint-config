d16e15a5567a2e5081e91d512e1412401c8b3178
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2018-03-16 14:51:19 -0300

    feat(plugins): New rules and reorder rules

--- plugins/promise.js
+++ plugins/promise.js
@@ -6,15 +6,18 @@ module.exports = {
     ],
     rules: {
         'promise/always-return': 'error',
-        'promise/no-return-wrap': 'error',
-        'promise/param-names': 'error',
+        'promise/avoid-new': 'error',
         'promise/catch-or-return': 'error',
+        'promise/no-callback-in-promise': 'error',
         'promise/no-native': 'off',
         'promise/no-nesting': 'error',
-        'promise/no-promise-in-callback': 'error',
-        'promise/no-callback-in-promise': 'error',
-        'promise/avoid-new': 'error',
         'promise/no-new-statics': 'error',
-        'promise/no-return-in-finally': 'error'
+        'promise/no-promise-in-callback': 'error',
+        'promise/no-return-in-finally': 'error',
+        'promise/no-return-wrap': 'error',
+        'promise/param-names': 'error',
+        'promise/prefer-await-to-callbacks': 'error',
+        'promise/prefer-await-to-then': 'error',
+        'promise/valid-params': 'error'
     }
 };
