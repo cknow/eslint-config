4a1f9d646af37018cacf45cec8cbea1d98541b14
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-07-12 10:37:36 -0300

    fix(parse-options): remove deprecated `experimentalObjectRestSpread`

--- config/parse-options.js
+++ config/parse-options.js
@@ -7,8 +7,7 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
         ecmaFeatures: {
-            jsx: true,
-            experimentalObjectRestSpread: true
+            jsx: true
         }
     }
 };
