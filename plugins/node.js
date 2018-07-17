6a24d64ab9c52e32dccaab1c4095e28835e7690a
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-07-17 08:07:05 -0300

    feat(node): added `node/prefer-global/process` rule

--- plugins/node.js
+++ plugins/node.js
@@ -54,6 +54,7 @@ module.exports = {
         }],
         'node/prefer-global/buffer': ['error', 'always'],
         'node/prefer-global/console': ['error', 'always'],
+        'node/prefer-global/process': ['error', 'always'],
         'node/prefer-global/url-search-params': ['error', 'always'],
         'node/prefer-global/url': ['error', 'always'],
         'node/process-exit-as-throw': 'error',
