b85d1c32ca0aa5505d18b76d7782899da0141d93
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-08-06 08:19:17 -0300

    feat(possible-errors): added `no-misleading-character-class` rule

--- rules/possible-errors.js
+++ rules/possible-errors.js
@@ -43,6 +43,7 @@ module.exports = {
             skipRegExps: true,
             skipTemplates: true
         }],
+        'no-misleading-character-class': 'error',
         'no-obj-calls': 'error',
         'no-prototype-builtins': 'error',
         'no-regex-spaces': 'error',
