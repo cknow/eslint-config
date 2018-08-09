e9313fe50065928d7703f59703b0ba67b837e8d9
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-08-09 15:00:59 -0300

    feat(jest): added `jest/expect-expect` rule

--- plugins/jest.js
+++ plugins/jest.js
@@ -11,6 +11,7 @@ module.exports = {
         'jest/consistent-test-it': ['error', {
             fn: 'test'
         }],
+        'jest/expect-expect': 'error',
         'jest/lowercase-name': ['error', {
             ignore: []
         }],
