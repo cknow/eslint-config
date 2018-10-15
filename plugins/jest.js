636019919e1f90f4f6da871f45c3c70e1dd78e41
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-10-15 08:40:38 -0300

    feat(plugins): added `jest/prefer-to-contain` rule

--- plugins/jest.js
+++ plugins/jest.js
@@ -41,6 +41,7 @@ module.exports = {
         'jest/prefer-strict-equal': 'error',
         'jest/prefer-to-be-null': 'error',
         'jest/prefer-to-be-undefined': 'error',
+        'jest/prefer-to-contain': 'error',
         'jest/prefer-to-have-length': 'error',
         'jest/require-tothrow-message': 'error',
         'jest/valid-describe': 'error',
