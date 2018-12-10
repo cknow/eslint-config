e7d25eec68f0a9643064236fc470692e74bc176c
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-12-10 11:15:08 -0200

    feat(plugins): add property `baseIndent` in `vue/html-indent` rule

--- plugins/vue.js
+++ plugins/vue.js
@@ -37,6 +37,7 @@ module.exports = {
         'vue/html-end-tags': 'error',
         'vue/html-indent': ['error', 4, {
             attribute: 1,
+            baseIndent: 1,
             closeBracket: 0,
             alignAttributesVertically: true,
             ignores: []
