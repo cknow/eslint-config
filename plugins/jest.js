0fbee43ac734bbd5e70b5afab06c1ac8ad0b44f8
Ricardo Gobbo de Souza <ricardo@datalogix.com.br>
2019-07-29 15:09:44 -0300

    feat(plugins): add rule `jest/no-if`

--- plugins/jest.js
+++ plugins/jest.js
@@ -32,6 +32,7 @@ module.exports = {
             ]
         }],
         'jest/no-identical-title': 'error',
+        'jest/no-if': 'error',
         'jest/no-jasmine-globals': 'error',
         'jest/no-jest-import': 'error',
         'jest/no-large-snapshots': ['error', {
